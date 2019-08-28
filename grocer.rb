0d2ef53033a911748d81218ae3a2ed59f26d8a69
Glenn A Doherty <gadz@earthlink.net>
2019-08-28 06:52:43 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,34 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  consol_cart = {}
+  cart.each do |cart_hash|
+    cart_hash.each do |cart_item, item_pricing|
+      if consol_cart[cart_item]
+        consol_cart[cart_item][:count] += 1
+      else
+        consol_cart[cart_item] = item_pricing
+        consol_cart[cart_item][:count] = 1
+      end
+    end
+  end
+  return consol_cart
 end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
+def apply_coupons (consol_cart, coupons)
+  coupons.each do |coupon_hash|
+    if consol_cart[coupon_hash[:item]]
+      new_item = coupon_hash[:item] + " W/COUPON"
+      if consol_cart[new_item]
+        consol_cart[new_item][:count] += coupon_hash[:num]
+      else consol_cart[new_item] = {
+        :price => coupon_hash[:cost]/coupon_hash[:num],
+        :clearance => consol_cart[coupon_hash[:item]][:clearance],
+        :count => coupon_hash[:num]
+      }
+      end
+      consol_cart[coupon_hash[:item]][:count] -= coupon_hash[:num]
+    end
+  end
+  return consol_cart
 end
